450943ef07e17d7e7fcf38966bab9719b5154539
whitfordr <whitfordR@berea.edu>
2019-09-15 19:49:55 -0400

    added changes to my website

--- poem.js
+++ poem.js
@@ -1,9 +1,10 @@
 function nextLine(line) {
 
-	var poem = new Array("<p onClick=nextLine(1)>There once was a maintainer on github</p>",
-"<p onClick=nextLine(2)>So many pull requests she did snub</p>",
-"<p onClick=nextLine(3)>So I forked the repo</p>",
-"<p onClick=nextLine(4)>Now they all send <em>me</em> changes, d'oh!</p>",
+	var poem = new Array("<p onClick=nextLine(1)>if you can't tell, mine is purple</p>",
+"<p onClick=nextLine(2)>Because purple is definetely the best</p>",
+"<p onClick=nextLine(3)>it doesnt show up very often naturally</p>",
+"<p onClick=nextLine(4)>but I think that just makes it more special</p>",
+<Button onClick=nextLine(5)>"index.getElementById('id1').style.color = 'purple'" </button>,
 "<p onClick=nextLine(0)>Said the maintainer, 'Welcome to the club!'</p>");
 
 	document.getElementById("line").innerHTML=poem[line];
